85c42e08852cf32abe2d271607bd580a9803f50b
Víctor Peña Romero <vic.pero@icloud.com>
2021-03-01 15:59:53 -0600

    jobs project done

--- 04-ninja-jobs/src/views/jobs/JobDetails.vue
+++ 04-ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,16 +1,28 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The route id is {{ id }}</p>
+  <div v-if="!!job">
+    <h1>{{ job.title }}</h1>
+    <p>The route id is {{ id }}</p>
+    <p>{{ job.details}}</p>
+  </div>
+  <div v-else>
+    <p>Loadin job details...</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ['id'],
-  // data() {
-  //   return {
-  //     id: this.$route.params.id
-  //   }
-  // }
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
 
