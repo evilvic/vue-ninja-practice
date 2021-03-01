1376fb651328fba6dfcd503f17f51fcea01188d1
Víctor Peña Romero <vic.pero@icloud.com>
2021-03-01 15:21:15 -0600

    404 and redirects

--- 04-ninja-jobs/src/App.vue
+++ 04-ninja-jobs/src/App.vue
@@ -4,8 +4,27 @@
     <router-link to="/about">About</router-link>
     <router-link to="/jobs">Jobs</router-link>
   </div>
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name:'Home' })
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -27,5 +46,12 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
