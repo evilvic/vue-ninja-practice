8f98a89a1e91c0d3ca23442a3b55d24c845824b8
Víctor Peña Romero <vic.pero@icloud.com>
2021-03-02 10:31:23 -0600

    add json data for exercise

--- 05-project-planner/src/App.vue
+++ 05-project-planner/src/App.vue
@@ -3,13 +3,16 @@
 </template>
 
 <style>
+body {
+  background: #f2f2f2;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   max-width: 600px;
   margin: 0 auto;
-  color: #2c3e50;
+  color: #555;
 }
 
 </style>
