cc2b18bae5bc95bb2e4756efa83efdd3d2956361
Víctor Peña Romero <vic.pero@icloud.com>
2021-03-03 11:25:48 -0600

    start project component

---
+++ 05-project-planner/src/components/SingleProject.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3>{{ project.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleProject',
+  props: ['project']
+}
+</script>
+
+<style>
+
+</style>
--- 05-project-planner/src/views/Home.vue
+++ 05-project-planner/src/views/Home.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="!!projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <single-project :project="project"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject'
+
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    SingleProject
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
